9a216daaa205c6ee80b945dcec8013e1824d7498
Daniel Finger <64750813+DanielZFinger@users.noreply.github.com>
2023-03-26 15:54:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -16,7 +16,11 @@ int main() {
     string month1;
     char day[100];
     string day1;
-    string time = "08:00:00Z</time>\n";
+    string time = "14:00:00Z</time>\n";
+
+    int startTime1;
+
+    int finishTime1;
     string fullTime;
     ofstream newFile;
     fstream myFile;
@@ -53,8 +57,21 @@ int main() {
     day1=day;
     cout<<"day is " << day1 << "\n";
 
-    fullTime="<time>"+year1+"-"+month1+"-"+day1+"T"+time;
+    cout<<"enter start time";
+    scanf("%d", &startTime1);
+    cout<<"start time is is " << startTime1 << "\n";
+
+    cout<<"enter finish time";
+    scanf("%d", &finishTime1);
+    cout<<"finishtime is is " << finishTime1 << "\n";
+
+    int totalTime= finishTime1-startTime1;
+    totalTime=totalTime*3600;
+
+    fullTime="<time>"+year1+"-"+month1+"-"+day1+"T";
     cout<<fullTime;
+
+
 
     //getting coords of start and finish
     string startCoordsLat;
@@ -120,7 +137,17 @@ int main() {
     string front;
 
     int   var_start = countStart-1;
-    int   var_fin = countFinish+15;
+    int   var_fin = countFinish+45;
+    int totalLines = var_fin-var_start;
+    cout<<"total Lines is "<<to_string(totalLines);
+    totalLines = totalLines/3;
+    cout<<"total lines post division is "<<to_string(totalLines);
+    int increments = totalTime/totalLines;
+    cout<<"increments is "<<to_string(increments);
+    int currentTime=startTime1;
+    int currentHour=startTime1;
+    int currentMinute=0;
+    int currentSecond=0;
 
     myFile.open("pct500.gpx", ios::in);
     if (myFile.is_open()) {
@@ -134,7 +161,28 @@ int main() {
                 full=full+data+"\n";
                 size_t found = data.find(checker);
                 if (found!=std::string::npos){
-                    full=full+fullTime;
+                    string tempHour = to_string(currentHour);
+                    string tempMinute = to_string(currentMinute);
+                    string tempSecond = to_string(currentSecond);
+                    if(currentHour<10){
+                        tempHour="0"+to_string(currentHour);
+                    }
+                    if(currentMinute<10){
+                        tempMinute="0"+to_string(currentMinute);
+                    }
+                    if(currentSecond<10){
+                        tempSecond="0"+to_string(currentSecond);
+                    }
+                    full=full+fullTime+tempHour+":"+tempMinute+":"+tempSecond+"Z"+"</time>";
+                    currentSecond=currentSecond+increments;
+                    if(currentSecond>59){
+                        currentMinute++;
+                        currentSecond=currentSecond-60;
+                    }
+                    if(currentMinute>59){
+                        currentHour++;
+                        currentMinute=currentMinute-60;
+                    }
                 }
             }
             else if(count>88293){
